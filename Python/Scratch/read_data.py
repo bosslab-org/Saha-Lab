48c8b920130e219ce16c15da17ba352d70112a6f
Alexander Farnum <88807728+xanderfarnum@users.noreply.github.com>
2022-10-31 15:27:12 -0400

    Updated input and labels matrix dimensions

--- Python/Scratch/read_data.py
+++ Python/Scratch/read_data.py
@@ -5,15 +5,18 @@ import matplotlib.pyplot as plt
 
 readpath = '/Users/alexanderfarnum/Documents/Code/Datasets/'
 mnist = 'mnist'
+cifar = 'cifar-10-batches/'
+np.random.seed(10)
 
 def one_hot(y):
-    one_hot_y = np.zeros((y.shape[0], np.max(y) + 1))
-    one_hot_y[np.arange(y.shape[0]), y] = 1
+    one_hot_y = np.zeros((y.shape[1], np.max(y) + 1))
+    one_hot_y[np.arange(y.shape[1]), y] = 1
     one_hot_y = one_hot_y.T
     return one_hot_y
 
+
 def flatten(x):
-    return np.reshape(x, (train_samples, x.shape[1] * x.shape[2]))
+    return np.reshape(x, (x.shape[0], x.shape[1] * x.shape[2]))
 
 
 def read_mnist(val_size=0.2):
@@ -39,13 +42,14 @@ def read_mnist(val_size=0.2):
     with gzip.open(X_train_path) as temp:
         temp.read(16)
         temp = temp.read(img_size * img_size * train_samples)
-        X_train = np.frombuffer(temp, dtype=np.uint8).astype(np.float32)
+        X_train = np.frombuffer(temp, dtype=np.uint8).astype(np.int64)
         X_train = X_train.reshape(train_samples, img_size*img_size).T
     print('Extracting y_train')
     with gzip.open(y_train_path) as temp:
         temp.read(8)
         temp = temp.read(1 * train_samples)
         y_train = np.frombuffer(temp, dtype=np.uint8).astype(np.int64)
+        y_train = y_train.reshape(train_samples, 1).T
     print('Extracting X_test')
     with gzip.open(X_test_path) as temp:
         temp.read(16)
@@ -59,9 +63,81 @@ def read_mnist(val_size=0.2):
         y_test = np.frombuffer(temp, dtype=np.uint8).astype(np.int64)
 
     X_val, X_train = np.split(X_train, [int(val_size*X_train.shape[1])], axis=1)
-    y_val, y_train = np.split(y_train, [int(val_size*y_train.shape[0])])
+    y_val, y_train = np.split(y_train, [int(val_size*y_train.shape[1])], axis=1)
 
     return X_train/255, y_train, X_val/255, y_val, X_test/255, y_test
+
+
+def read_cifar(val_size=0.2):
+    """Import and preprocess CIFAR-10 dataset.
+    Accepts optional validation size, else val_size = 0.2
+    """
+    source = 'https://www.cs.toronto.edu/~kriz/cifar.html'
+    test_readpath = 'testdata'
+    def unpickle(file):
+        import pickle
+        with open(file, 'rb') as fo:
+            dict = pickle.load(fo, encoding='latin1')
+        return dict
+    X_train = []
+    y_train = []
+    for train_filepath in os.listdir(readpath + cifar):
+        if train_filepath.startswith("traindata"):
+            data_temp = unpickle(readpath + cifar + train_filepath)
+            X_train.append(data_temp['data'])
+            y_train.append(data_temp['labels'])
+    X_train = np.array(X_train)
+    X_train = X_train.reshape(X_train.shape[0]*X_train.shape[1], X_train.shape[2])
+    y_train = np.array(y_train)
+    y_train = y_train.reshape(y_train.shape[0]*y_train.shape[1])
+
+    np.random.shuffle(X_train)
+    np.random.shuffle(y_train)
+
+    X_val, X_train = np.split(X_train, [int(val_size*X_train.shape[0])])
+    y_val, y_train = np.split(y_train, [int(val_size*y_train.shape[0])])
+
+    data_temp = unpickle(readpath + cifar + test_readpath)
+    X_test = np.array(data_temp['data'])
+    y_test = np.array(data_temp['labels'])
+
+    return X_train/255, y_train, X_val/255, y_val, X_test/255, y_test
+
+
+def read_locust_data():
+    import scipy.io  # v7
+    data = scipy.io.loadmat('/Users/XanderFarnum/Documents/NN/Sandbox/Datasets/Locust/Cell_Culture_RMS_Master.mat')
+
+    # import h5py  # v7.3
+    # f = h5py.File('somefile.mat', 'r')
+    # data = f.get('data/variable1')
+    # data = np.array(data)  # For converting to a NumPy array
+
+    values = [v for k, v in data.items() if k.endswith('RMS_data_filt')]
+    # data_dict = {keys: values for (key, value) in data.items() if key.endswith('RMS_data_filt')}
+
+    num_neurons = values[0][:, 0, 0, 0].size
+    num_trials = values[0][0, 0, :, 0].size
+    num_samples = values[0][0, 0, 0, :].size
+    num_stimuli = len(values)
+
+    data = np.zeros((num_neurons, num_trials, num_samples, num_stimuli))
+    labels = np.zeros((num_neurons, num_stimuli))
+    for i in range(len(values)):
+        data[:, :, :, i] = values[i].mean(axis=1)
+        labels[:, i] = i
+
+    data = data.transpose((0, 3, 2, 1)).reshape((num_neurons * num_stimuli, num_samples, num_trials)) / data.max()
+
+    # data = data.transpose((0, 1, 3, 2)).reshape((num_neurons * num_trials * num_stimuli, num_samples)) / data.max()
+    labels = labels.reshape((num_neurons * num_stimuli, 1))
+
+    # np.random.shuffle(data)
+    # np.random.shuffle(labels)
+    # X_test, X_train = np.split(data, [int(val_size*data.shape[0])])
+    # y_test, y_train = np.split(labels, [int(val_size*labels.shape[0])])
+
+    return data, labels
 
 
 def spiral_data(n, m, k):
