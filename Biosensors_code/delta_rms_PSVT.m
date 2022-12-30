733f1209cc2eb8405b244cdefe9e04b3b748dc81
bosslab2020 <121509670+bosslab2020@users.noreply.github.com>
2022-12-30 14:02:37 -0500

    Delete Biosensors_code directory

--- Biosensors_code/delta_rms_PSVT.m
+++
@@ -1,9 +0,0 @@
-function delta_rms_PSVT(data, Colors, Odorants)
-
-figure('Units','normalized','Position',[0 0 0.75 1]); hold on;
-for cycle_stimuli = 1:size(data,4)
-    plot(1:size(data,2),mean(data(:,:,:,cycle_stimuli),[1 3]),'Color',Colors(cycle_stimuli,:))
-end
-legend(Odorants,'location','eastoutside','FontSize',16,'FontWeight','Bold');
-
-return
